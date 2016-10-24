324c7a38971b150aa9807110b3b57bc003b2e985
LincolnFrederico <lcnfred333@gmail.com>
2016-10-24 20:31:32 -0200

    Lincoln
    - Tirar o CSS interno
    - Alteração de mensagem de erro no arquivo upload para caso a imagem seja de uma extensão diferente da tratada no código.
    - Add comentários nó código.

---
+++ foto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html lang="pt-br">
+	<head>
+		<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+		<meta html lang="pt-br">
+		<meta charset='UTF-8'>
+		<title>Formulario envio de imagem</title>
+		
+		<script language="javascript" type="text/javascript">
+			
+			
+
+		</script>
+	</head>
+		
+	<body>
+		
+		<form  action="upload.php" method="post" enctype="multipart/form-data">  
+
+			<label for="foto">Envie seu <strike>Nudes</strike> </label><br><br><br> 
+			
+			
+			<input type="file"  name="foto" ><br><br>
+			
+			
+			
+			<input type="submit" value="Enviar Foto!"> 
+			
+			
+		</form>
+
+
+		
+	
+		
+	</body>
+</html>
